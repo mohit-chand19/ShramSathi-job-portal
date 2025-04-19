63c02df5a8de7bbb5342a63a2f47cda2271dc523
Mohit Chand <chandmohit002@gmail.com>
2025-04-19 21:41:12 +0545

    job model controller and route setup

--- backend/controllers/job.controller.js
+++ backend/controllers/job.controller.js
@@ -1,5 +1,6 @@
 import { Job } from "../models/job.model.js";
 
+// admin or recruiter post job
 export const postJob = async (req,res) => {
     try {
         const {title, description, requirements, salary, location, jobType, experience, position, companyId} = req.body;
@@ -33,9 +34,59 @@ export const postJob = async (req,res) => {
     }
 }
 
+// freelancer or job seeker can see all jobs posted
 export const getAllJobs = async (req,res) => {
     try {
-        
+        const keyword = req.query.keyword || "";
+        const query = {
+            $or:[
+                {title:{$regex:keyword, $options:"i"}},
+                {description:{$regex:keyword, $options:"i"}},
+            ]
+        };
+        const jobs = await Job.find(query)
+        if(!jobs){
+            return res.status(404).json({
+                message:"Jobs not found.",
+                success:false
+            });
+        }
+        return res.status(200).json({
+            jobs,
+            success:true
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// freelancer or job seeker can see jobs by searching
+export const getJobById = async (req, res) => {
+    try {
+        const jobId = req.params.id;
+        const job = await Job.findById(jobId);
+        if(!job){
+            return res.status(404).json({
+                message:"Jobs not found.",
+                success:false
+            });
+        }
+        return res.status(200).json({job, success:true});
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+// admin to see jobs that he created
+export const getAdminJobs = async (req, res) => {
+    try {
+        const adminId = req.id;
+        const jobs = await Job.find({created_by:adminId});
+        if(!jobs){
+            return res.status(404).json({message:"Jobs not found",success:false});
+        }
+        return res.status(200).json({jobs,success:true});
     } catch (error) {
         console.log(error);
     }
