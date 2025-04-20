83c4791d275ab5fb01a534c59e5c766417161bd5
Mohit Chand <chandmohit002@gmail.com>
2025-04-20 17:46:42 +0545

    Application controller and route

--- backend/models/job.model.js
+++ backend/models/job.model.js
@@ -42,9 +42,9 @@ const jobSchema = new mongoose.Schema({
         ref: 'User',
         required: true
     },
-    application:[{
+    applications:[{
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'Application', 
+        ref: 'Application',
     }]
 },{timestamps:true});
 export const Job = mongoose.model("Job", jobSchema);
